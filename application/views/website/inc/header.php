47208e2efe9dcb27e4ed27c9d56232c1bcbea650
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-11-29 17:59:10 +0530

    Update header.php

--- application/views/website/inc/header.php
+++ application/views/website/inc/header.php
@@ -10,14 +10,15 @@
                     <a href="<?php echo base_url('/')?>" style="text-decoration: none;"><h2 style="font-family: 'Cinzel', serif; text-align: center; margin-top:30px;color: rgb(35 97 138);"> Bodhiwire</h2></a>
                 </div>
                 <div class="col-md-4 mb-3">
+                    <a href="<?php echo base_url('/')?>" style="text-decoration: none;"><p style="font-family: 'Cinzel', serif; text-align: center; margin-top:10px;color: rgb(35 97 138);"> A Vanman Communications Initiative</p></a>
                     <div class="input-group rounded">
-                      <input type="search" class="form-control rounded" style="margin-top:30px;" placeholder="Search" aria-label="Search"
+                      <input type="search" class="form-control rounded" style="margin-top:10px;" placeholder="Search" aria-label="Search"
                       aria-describedby="search-addon" />
-                      <span class="input-group-text border-0" style="margin-top:30px; margin-right: 7px;" id="search-addon">
+                      <span class="input-group-text border-0" style="margin-top:10px; margin-right: 7px;" id="search-addon">
                         <i class="fas fa-search"></i>
                       </span>
-                      <button type="button" class="btn btn-sm btn-space btn-outline-dark " data-mdb-ripple-color="dark" style="float:right;margin-top:30px; margin-right: 7px;">Sign In</button>
-                    <button type="button"  style="float:right;margin-top:30px;" class="btn btn-sm btn-dark ">Register</button>
+                      <button type="button" class="btn btn-sm btn-space btn-outline-dark " data-mdb-ripple-color="dark" style="float:right;margin-top:10px; margin-right: 7px;">Sign In</button>
+                    <button type="button"  style="float:right;margin-top:10px;" class="btn btn-sm btn-dark ">Register</button>
                     </div>
                    
                     
