af4800bc6a787b6733daa8ea11519d7ecf6841f4
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-06 16:38:45 +0530

    update

--- application/views/website/inc/top-header.php
+++ application/views/website/inc/top-header.php
@@ -36,7 +36,9 @@
 
           <link rel="preconnect" href="https://fonts.googleapis.com">
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap" rel="stylesheet">
+          <link href="https://fonts.googleapis.com/css2?family=Abel&family=Arvo&family=Cinzel:wght@500&family=Heebo:wght@100;300;400;800&family=M+PLUS+1p:wght@900&family=Readex+Pro:wght@500;600&display=swap" rel="stylesheet">
+          <!-- <link href="https://fonts.googleapis.com/css2?family=Abel&family=Arvo&family=Cinzel:wght@500&family=Heebo:wght@100;300;400;800&family=Readex+Pro:wght@500;600&display=swap" rel="stylesheet"> -->
+          <!-- <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap" rel="stylesheet"> -->
 
     
 
