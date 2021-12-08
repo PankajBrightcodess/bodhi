dedef1312c683a05358541aedc87aa8c83e4258f
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2021-12-08 09:43:37 +0530

    update

--- application/views/website/inc/header.php
+++ application/views/website/inc/header.php
@@ -76,10 +76,15 @@
                                   </div>
                                   <div class="col-md-9">
                                      <div class="row ">
-                                            <!-- <div class="col-md-6"><h2>Hello</h2></div>
-                                            <div class="col-md-6"><h2>Hello</h2></div>
-                                            <div class="col-md-6"><h2>Hello</h2></div>
-                                            <div class="col-md-6"><h2>Hello</div> -->
+                                           <?php
+                                           // echo PRE;
+                                           //  print_r($gettopnews);
+                                            
+
+
+
+
+                                           ?>
                                            
                                         </div>
                                     </div>
