e0f6ea12401751b9a96690b89ecbdb3e59d25299
satya-office <Satyaki.bhattacharya@brightcodess.com>
2022-04-22 11:32:32 +0530

    sai

--- application/views/website/india/detailnews.php
+++ application/views/website/india/detailnews.php
@@ -54,7 +54,7 @@
             <a class="social-links-item twitter-icon" title="Twitter" href="https://twitter.com/">Twitter</a>
             <a class="social-links-item facebook-icon" title="Facebook" href="https://www.facebook.com/">Facebook</a>
             <a class="social-links-item linkedin-icon" title="Youtube" href="https://www.linkedin.com/">LinkedIn</a>
-            <a class="social-links-item rss-icon" title="Github" href="#nogo">RSS</a>
+            <a class="social-links-item rss-icon" title="Youtube" href="#nogo">RSS</a>
             <a class="social-links-item mail-icon" title="Google Groups" href="#nogo">Email</a>
         </div>
 				  		 </div>
@@ -115,6 +115,145 @@
    </div>
 
 </div>
+
+
+
+
+
+
+
+
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="accordion" id="accordionExample">
+  				<div class="accordion-item">
+    				<h2 class="accordion-header" id="headingOne">
+      				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<!-- <section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="accordion" id="accordionExample">
+  				
+  				<div class="card">
+  			  	<div class="card-header" id="headingOne">
+  			    	<h2 class="mb-0">
+  			      	<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Support Bodhi Wire<span class="btn btn-secondary" style="float:right;font-size: 19px;border-radius: 20px;">₹2400</span>
+  			      	</button>
+  			    	</h2>
+  			  	</div>
+						<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+  			    	<div class="card-body">The founding premise of The Wire is this: if good journalism is to survive and thrive, it can only do so by being both editorially and financially independent.This means relying principally on contributions from readers and concerned citizens who have no interest other than to sustain a space for quality journalism. For any query or help write to us at bodhiwire@gmail.com
+  			    		<p>I would like to contribute.</p>
+  			    		<div class="row">
+  			    			<div class="col-md-4">
+  			    				<p class="btn btn-primary btn-block">Once</p>
+  			    			</div>
+  			    			<div class="col-md-4">
+  			    				<p class="btn btn-primary btn-block">Monthly</p>
+  			    			</div>
+  			    			<div class="col-md-4">
+  			    				<p class="btn btn-primary btn-block">Yearly</p>
+  			    			</div>
+  			    			<p>Select Amount</p>
+  			    			<div class="col-md-3">
+  			    				<p class="btn btn-primary btn-block">Once</p>
+  			    			</div>
+  			    			<div class="col-md-3">
+  			    				<p class="btn btn-primary btn-block">Monthly</p>
+  			    			</div>
+  			    			<div class="col-md-3">
+  			    				<p class="btn btn-primary btn-block">Yearly</p>
+  			    			</div>
+  			    			<div class="col-md-3">
+  			    				<p class="btn btn-primary btn-block">Yearly</p>
+  			    			</div>
+
+  			    			
+
+  			    		</div>
+  			    		<p class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Continue</p>
+  			    	</div>
+  			  	</div>
+  				</div>
+  				
+  				<div class="card">
+    				<div class="card-header" id="headingTwo">
+      				<h2 class="mb-0">
+        				<button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Collapsible Group Item #2
+        				</button>
+      				</h2>
+    				</div>
+    				<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      				<div class="card-body">Some placeholder content for the second accordion panel. This panel is hidden by default.</div>
+    				</div>
+  				</div>
+  				
+  				<div class="card">
+    				<div class="card-header" id="headingThree">
+      				<h2 class="mb-0">
+        				<button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Collapsible Group Item #3
+        				</button>
+      				</h2>
+    				</div>
+    				<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      				<div class="card-body">And lastly, the placeholder content for the third and final accordion panel. This panel is hidden by default.</div>
+    				</div>
+  				</div>
+				
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script> -->
+
 <script>
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
