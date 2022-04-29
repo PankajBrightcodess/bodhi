f410f0e2f457950a89f9d301a199bb4c692e9dec
sauravdevp <saurav.kumar@brightcodess.com>
2022-04-29 18:07:47 +0530

    Update addnews.php

--- application/views/pages/admin/addnews.php
+++ application/views/pages/admin/addnews.php
@@ -127,67 +127,6 @@
                 </div>
                 <?php echo form_close();?>
            </div>
-            <div class="col-md-12">
-                <hr>
-            </div>
-            <?php 
-                if($_SESSION['role']=='admin'){
-                    ?>
-                    <div class="col-md-12 table-responsive" >
-                <table class="table data-table stripe hover nowrap table-bordered">
-                    <thead>
-                        <tr style="background-color: #23618a !important; color:white;">    
-                            <th>S.no</th>
-                            <th>ENTRY ID</th> 
-                            <th>MENU NAME</th> 
-                            <th>SUBMENU NAME</th> 
-                            <th>IMAGE</th>   
-                            <th width="20%">TITTLE</th>  
-                            <th>By Line</th>
-                            <!-- <th>SLUG</th>  -->
-                            <th>Published</th>                                         
-                            <th>Action</th>                                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i=0; if(!empty($result)){
-                            foreach($result as $val){ $i++;$id=$val['id']; ?>
-                        <tr>
-                            <td ><?= $i?></td>
-                            <td ><?= $val['id']?></td>
-                            <td><?php echo $menu = $this->Account_model->getmenuname($val['menu_id']);?></td>
-                            <td><?php echo $submenu = $this->Account_model->getsubmenuname($val['submenu_id']);?></td>
-                            <!-- <td ><= $val['menu_id']?></td> -->
-                            <!-- <td ><= $val['submenu_id']?></td> -->
-                            <td><img src="<?php echo file_url($val['image']); ?>" alt="Image" class="img-thumbnail" alt="Responsive image"></td>
-                            <td width="20%"><?= $val['tittle']?></td>
-                            
-                            <!-- <td ><?= $val['slug']?></td> -->
-                            <td ><?= $val['byline']?></td>
-                            <td style="text-align: center;"> <?php
-                                    if($val['published']==1){
-                                        ?><button class="btn btn-info btn-xs published" value="<?php echo $val['id'];?>"><i class="fa fa-check"></i></button><?php
-                                    }else{
-                                        ?><button class="btn btn-warning btn-xs published" value="<?php echo $val['id'];?>"><i class="fa fa-window-close-o" aria-hidden="true"></i></button><?php
-                                    }
-                        ?> </td>
-                            <td><span class="float-right">
-                            <button class="btn btn-danger btn-xs delete" value="<?php echo $val['id'];?>"><i class="fa fa-trash"></i></button> 
-                            <a href="<?php echo base_url('home/edit_news/?id='.$val['id'])?>"><button type="button" class="btn btn-success btn-xs"><i class="fa fa-edit"></i></button></a>
-                           <!-- <button type="button" class="btn btn-success btn-xs updt" data-toggle="modal" data-id="<?php echo $val['id'];?>" data-menu="<?php echo $val['menu_id'];?>" data-submenu="<?php echo $val['submenu_id'];?>" data-tittle="<?php echo $val['tittle'];?>" data-byline="<?php echo $val['byline'];?>"  data-straplines="<?php echo $val['straplines'];?>" data-image="<?php echo $val['image'];?>" data-img_caption="<?php echo $val['img_caption'];?>" data-topnews="<?php echo $val['top_news_status'];?>" data-target=".bd-example-modal-lg"><i class="fa fa-edit"></i></button> -->
-
-                           <!--  <button class="btn btn-info btn-xs duplicate" type="button" data-dupid=""><i class="fa fa-network-wired"></i></button> -->
-                            </span></td>
-                        </tr>
-                        <?php } } ?> 
-                    </tbody>
-                </table>
-            </div>
-                    <?php
-                }
-
-
-            ?>
         	
         </div>
     </div>
