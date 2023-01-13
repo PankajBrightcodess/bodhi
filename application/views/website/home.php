71e8f851f486d97869792871b8885bd0cfd22e63
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-13 14:39:13 +0530

    ss

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -280,7 +280,7 @@
         		       </div> -->
         		       <div class="col-md-3 mb-3">
 					  		<div class="card home-india cardsec">
-							  <a href="<?php echo base_url('website/detailnewsview/' . $value['slug']);?>" style="color: black;" class="img-hover"><img width="100%">  src="<?php echo base_url();?><?php echo $value['image'] ?>" alt="Card image cap"></a>
+							  <a href="<?php echo base_url('website/detailnewsview/' . $value['slug']);?>" style="color: black;" class="img-hover"><img  class="img-fluid" src="<?php echo base_url();?><?php echo $value['image'] ?>" alt="Card image cap"></a>
 							  
 							  <div class="card-body">
 							  	<div class="row">
