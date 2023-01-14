676c1a577464c77212ccc6d0dfe30d9c7976b1cc
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-14 15:05:32 +0530

    Update home.php

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -217,7 +217,7 @@
              	<div class="col-md-12">
              	   <div class="card">
              		 <iframe  class="iframedit" src="https://www.youtube.com/embed/NDy5ebew7gI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                        <p class="viedo" >Security Breach in pm Modi's Karnataka Roadshow</p><hr>
+                        <p class="viedo" >Security Breach in pm Modi's Karnataka Roadshow</p>
              	    </div>
              	</div>
             </div>
