f6afa0a5653d5d30074897a4c61505b3200e140b
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-14 14:53:53 +0530

    ww

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -200,7 +200,7 @@
              <section>
              	<div class="container">
              		<div class="row">
-             			<div class="col-md-12">
+             			<div class="col-md-3">
              			  <div class="card">
              			  	 <iframe  class="iframedit" src="https://www.youtube.com/embed/NDy5ebew7gI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                 <p class="viedo" >Security Breach in pm Modi's Karnataka Roadshow</p><hr>
