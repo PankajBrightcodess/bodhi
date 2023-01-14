8927033ccc1bce2c03928184528eb364d00d1275
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-14 15:07:52 +0530

    ss

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -212,7 +212,7 @@
 
 
 
-	<div class="col-md-3 col-sm-12 mb-3">
+	<div class="col-md-3 col-sm-12 mb-3  aaf">
 			<div class="row">
              	<div class="col-md-12">
              	   <div class="card">
