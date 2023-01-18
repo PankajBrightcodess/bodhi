4d8e1b60c3d91c2b88f86ec711225a560b80a840
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-18 17:40:34 +0530

    Update home.php

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -219,8 +219,8 @@
              	<div class="col-md-12">
              	   <div class="card">
              	   	<h2 class="tn">Top News</h2>
-             		 <iframe  src="https://www.youtube.com/embed/7gnO9GwkwkM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                        <center><p class="video" >Coming soon...</p></center>
+             	   	 <iframe class="iframedit" src="https://www.youtube.com/embed/7gnO9GwkwkM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                      <center><p class="video" >Coming soon...</p></center>
              	    </div>
              	</div>
             </div>
