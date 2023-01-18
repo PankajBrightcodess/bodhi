e1bd39c3c193a581f9f11e9761d9b045beab3d14
tipubss <88927087+tipubss@users.noreply.github.com>
2023-01-18 17:29:15 +0530

    sa

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -219,7 +219,7 @@
              	<div class="col-md-12">
              	   <div class="card">
              	   	<h2 class="tn">Top News</h2>
-             		 <iframe  class="iframedit" src="https://www.youtube.com/embed/NDy5ebew7gI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+             		 <iframe  class="iframedit" src="https://www.youtube.com/watch?v=7gnO9GwkwkM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         <p class="video" >Security Breach in pm Modi's Karnataka Roadshow</p>
              	    </div>
              	</div>
