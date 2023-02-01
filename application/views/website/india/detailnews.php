002a2bbfa97cd2a8006cd16ef09ec2b72b7c6a3d
tipubss <88927087+tipubss@users.noreply.github.com>
2023-02-01 15:33:52 +0530

    saa

--- application/views/website/india/detailnews.php
+++ application/views/website/india/detailnews.php
@@ -56,7 +56,7 @@
                  
 
 
-                  <a target="_blank" href="http://www.facebook.com/sharer.php?s=100&p[summary]=<?= $result['tittle']; ?>&p[url]=<?= base_url('website/detailnewsview/'.$result['tittle']); ?>" class="social-links-item facebook-icon"></a>
+                  <a target="_blank" href="http://www.facebook.com/sharer.php?s=100&p[summary]=<?= $result['news']; ?>&p[url]=<?= base_url('website/detailnewsview/'.$result['tittle']); ?>" class="social-links-item facebook-icon"></a>
 
 
                   <!-- <a class="social-links-item facebook-icon" title="Facebook" href="https://www.facebook.com/">Facebook</a> -->
