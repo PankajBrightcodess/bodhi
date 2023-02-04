b8c93b27a537fa639a965ca4503bd68c6e90c06c
tipubss <88927087+tipubss@users.noreply.github.com>
2023-02-04 15:41:46 +0530

    ss

--- change.sql
+++ change.sql
@@ -12,7 +12,6 @@ ALTER TABLE `tmp_payment` DROP `email`;
 ALTER TABLE `tmp_payment` DROP `currency`, DROP `error_desc`;
 
 
-
 INSERT INTO `tmp_donation` (`tranid`, `amount`, `name`, `last`, `email`, `type`, `phone`, `pan`, `checkvalue`) VALUES ('230204-5954', '500', 'Brightcode', 'Tiwari', 'brightcodesoftware@gmail.com', 'Monthly', '07808360437', 'PG45354K', '1')
 
 INSERT INTO `tmp_payment` (`amount`, `tranid`, `pay_token_id`, `payment_id`, `full_name`, `email`, `currency`, `vpa`, `payment_status`, `error_desc`, `phone`) VALUES ('500.00', '230204-94679', 'sb_pt_BVvT8LaGkVg68mN', 'sb_py_BVvT8VQP61h0YbG', 'Brightcode', NULL, 'INR', 'dfs@okaxis', 'failed', NULL, '07808360437')
