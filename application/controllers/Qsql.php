3f625e88611ad679dc7e915f64de819a7991fe12
tipubss <88927087+tipubss@users.noreply.github.com>
2023-02-04 16:00:44 +0530

    Update Qsql.php

--- application/controllers/Qsql.php
+++ application/controllers/Qsql.php
@@ -10,32 +10,32 @@ class Qsql extends CI_Controller{
 			$this->template->load('pages','query',$data);
 		}
 	
-	public function goquery(){
-		$query=$this->input->post('query');
-		$str=explode(";",$query);
-		$count=count($str);
-		for($i=0;$i<$count-1;$i++){
-			if(!empty($str[$i])){
-				$run[]=$this->db->query($str[$i]);
-				}
-			}
-			$ok=count($run);
-			$x=0;
-			for($i=0;$i<$ok;$i++){
-				if(empty($run)){
-					++$x;	
-					}
-				}
-		if($run){
-			$this->session->set_flashdata('msg',"Query Executed Successfully");
-			}
-			else{
-				$this->ession->set_flashdata('err_msg',"Error Please Try Again $x Not Executed");
-				}
-			redirect($_SERVER['HTTP_REFERER']);
-		}
+	// public function goquery(){
+	// 	$query=$this->input->post('query');
+	// 	$str=explode(";",$query);
+	// 	$count=count($str);
+	// 	for($i=0;$i<$count-1;$i++){
+	// 		if(!empty($str[$i])){
+	// 			$run[]=$this->db->query($str[$i]);
+	// 			}
+	// 		}
+	// 		$ok=count($run);
+	// 		$x=0;
+	// 		for($i=0;$i<$ok;$i++){
+	// 			if(empty($run)){
+	// 				++$x;	
+	// 				}
+	// 			}
+	// 	if($run){
+	// 		$this->session->set_flashdata('msg',"Query Executed Successfully");
+	// 		}
+	// 		else{
+	// 			$this->ession->set_flashdata('err_msg',"Error Please Try Again $x Not Executed");
+	// 			}
+	// 		redirect($_SERVER['HTTP_REFERER']);
+	// 	}
 	
 	
-	}
+	// }
 
 ?>
