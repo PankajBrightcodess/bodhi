8605e940634cdb9d23e1db6ea9a600f9086c7ac3
tipubss <88927087+tipubss@users.noreply.github.com>
2023-02-04 15:19:11 +0530

    ss

--- change.sql
+++ change.sql
@@ -3,3 +3,12 @@ ALTER TABLE `tmp_donation` ADD `added_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIM
 CREATE TABLE `db_bodhiwire`.`tmp_payment` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `full_name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `phone` VARCHAR(20) NOT NULL , `amount` FLOAT(14,2) NOT NULL , `currency` VARCHAR(100) NOT NULL , `payment_id` TEXT NOT NULL , `error_desc` TEXT NOT NULL , `payment_status` VARCHAR(200) NOT NULL , `tranid` TEXT NOT NULL , `pay_token_id` TEXT NOT NULL , `added_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 ALTER TABLE `tmp_payment` ADD `vpa` TEXT NOT NULL AFTER `pay_token_id`;
 ALTER TABLE `tmp_donation` ADD `tranid` VARCHAR(255) NOT NULL AFTER `pan`;
+
+
+
+-------not done---------
+ALTER TABLE `tmp_donation` CHANGE `pan` `pan` VARCHAR(255) NOT NULL;
+
+
+
+INSERT INTO `tmp_donation` (`tranid`, `amount`, `name`, `last`, `email`, `type`, `phone`, `pan`, `checkvalue`) VALUES ('230204-5954', '500', 'Brightcode', 'Tiwari', 'brightcodesoftware@gmail.com', 'Monthly', '07808360437', 'PG45354K', '1')
